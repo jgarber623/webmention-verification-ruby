29afcb0ead350f9d4ae049df52ec4d071cd6ff52
Jason Garber <jason@sixtwothree.org>
2019-04-25 12:20:43 -0400

    Refactor response method exception handling

--- lib/webmention/verification/client.rb
+++ lib/webmention/verification/client.rb
@@ -21,16 +21,11 @@ module Webmention
       end
 
       def response
-        @response ||= HTTP.follow.headers(HTTP_HEADERS_OPTS).timeout(
-          connect: 10,
-          read: 10
-        ).get(source_uri)
-      rescue HTTP::ConnectionError => exception
-        raise ConnectionError, exception
-      rescue HTTP::TimeoutError => exception
-        raise TimeoutError, exception
-      rescue HTTP::Redirector::TooManyRedirectsError => exception
-        raise TooManyRedirectsError, exception
+        @response ||= HTTP.follow.headers(HTTP_HEADERS_OPTS).timeout(connect: 10, read: 10).get(source_uri)
+      rescue HTTP::ConnectionError,
+             HTTP::TimeoutError,
+             HTTP::Redirector::TooManyRedirectsError => exception
+        raise Webmention::Verification.const_get(exception.class.name.split('::').last), exception
       end
 
       def source_uri
