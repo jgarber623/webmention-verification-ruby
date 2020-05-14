71985d12a9cad8882b60301fde2fe579480e6555
Jason Garber <jason@sixtwothree.org>
2020-05-14 16:05:39 -0400

    Remove LOAD_PATH modification

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,8 +4,6 @@ require 'webmock/rspec'
 
 require 'webmention/verification'
 
-$LOAD_PATH.unshift File.expand_path('../lib', __dir__)
-
 Dir.glob(File.join(Dir.pwd, 'spec', 'support', '**', '*.rb')).sort.each { |f| require f }
 
 RSpec.configure do |config|
