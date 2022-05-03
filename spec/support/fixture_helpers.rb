92f7886c2e01155df28420080709934ed83ee5ff
Jason Garber <jason@sixtwothree.org>
2022-05-03 10:22:31 -0400

    Fix line length

--- spec/support/fixture_helpers.rb
+++ spec/support/fixture_helpers.rb
@@ -4,6 +4,6 @@ module FixtureHelpers
   def read_fixture(uri, format = 'html')
     matches = uri.match(%r{^https?://(?<domain>.*?\..*?\.com)(?<path>.*)$})
 
-    File.read(File.join(Dir.pwd, 'spec', 'support', 'fixtures', matches['domain'], format, "#{matches['path']}.#{format}"))
+    File.read(File.join(Dir.pwd, 'spec/support/fixtures', matches['domain'], format, "#{matches['path']}.#{format}"))
   end
 end
